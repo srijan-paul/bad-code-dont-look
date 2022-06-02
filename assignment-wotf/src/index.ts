e8d1137e62473b9c1bccceaf5a0ca5f2c91ddfa1
Srijan Paul <srijan.paul@deepsource.io>
2022-06-02 09:38:42 +0530

    assignment wotf

---
+++ assignment-wotf/src/index.ts
@@ -0,0 +1,112 @@
+import Express from 'express';
+import path from 'path';
+import pg from 'pg';
+
+const pool = new pg.Pool({
+  user: 'me',
+  host: 'localhost',
+  database: 'student',
+  password: 'password',
+  port: 5432
+})
+
+
+const app = Express()
+app.use('/', Express.static(path.join(__dirname, 'public')))
+app.use(Express.json())
+app.use(Express.urlencoded({ extended: true }))
+
+interface StudentDetails {
+  firstName: string;
+  lastName: string;
+  email: string;
+  regdNo: string;
+  branch: string;
+}
+
+function tryCall<T>(fn: () => T): [T | undefined, unknown?] {
+  try {
+    return [fn()]
+  } catch (err: unknown) {
+    return [, err]
+  }
+}
+
+async function tryCallAsync<T>(fn: () => Promise<T>): Promise<[T | undefined, unknown?]> {
+  try {
+    return [await fn()]
+  } catch (err: unknown) {
+    return [, err]
+  }
+}
+
+async function runQuery_(query: string, params: string[] = []): Promise<pg.QueryResult<any>> {
+  return new Promise((resolve, reject) => {
+    pool.query(query, params, (err, result) => {
+      if (err) { reject(err); return; }
+      resolve(result);
+    })
+  });
+}
+
+async function runQuery(query: string, params: string[] = []) {
+  return tryCallAsync(async () => await runQuery_(query, params))
+}
+
+app.get("/api/student", async (_, res) => {
+  const [result, err] = await runQuery('SELECT * FROM students');
+  if (result) return res.status(200).json(result.rows)
+
+  res.status(500);
+  res.end();
+  throw err;
+})
+
+app.get("/api/teacher", async (_, res) => {
+  const [result, err] = await runQuery('SELECT * FROM teachers');
+  if (result) return res.status(200).json(result.rows)
+
+  res.status(500);
+  res.end();
+  throw err;
+})
+
+app.post('/api/student', async (req, res) => {
+  const { firstname, lastname, email, branch } = req.body;
+  const insertQuery = `INSERT INTO 
+    students (firstname, lastname, email, branch)
+    VALUES ('${firstname}', '${lastname}', '${email}', '${branch}')`;
+  const [result, err] = await runQuery(insertQuery);
+  if (result) return res.status(200);
+  res.status(500);
+  res.send();
+  throw err;
+})
+
+app.post('/api/teacher', async (req, res) => {
+  const { name, email, subject, phone } = req.body;
+
+  const insertQuery = `INSERT INTO 
+    teachers (name, email, subject, phone)
+    VALUES ('${name}', '${email}', '${subject}', '${phone}')`;
+
+  const [result, err] = await runQuery(insertQuery);
+  if (result) return res.status(200);
+
+  res.status(500);
+  res.send();
+  throw err;
+})
+
+// im sorry
+app.post('/api/student-delete', async (req, res) => {
+  const { id } = req.body
+  const deleteQuery = `DELETE FROM STUDENTS where id = ${id};`
+  const [result, err] = await runQuery(deleteQuery)
+  if (result) return res.status(200)
+  res.status(500)
+  res.end();
+  throw err
+})
+
+app.listen(3000, () => console.log('Listening on port 5000'))
